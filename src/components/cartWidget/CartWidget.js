0e1ccfb3334ec44bf98f89b224cbf479af2dcdca
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-04 18:03:21 -0400

    Create CartWidget.js

---
+++ src/components/cartWidget/CartWidget.js
@@ -0,0 +1,17 @@
+// rcc
+
+import React, { Component } from 'react'
+import "./cartWidget.css"
+
+export default class cartWidget extends Component {
+    render() {
+        return (
+            <div className="carro">
+                <img src="https://www.nicepng.com/png/full/261-2611207_cart-add-to-cart-icon-png.png" />
+            </div>
+        )
+    }
+}
+
+
+
