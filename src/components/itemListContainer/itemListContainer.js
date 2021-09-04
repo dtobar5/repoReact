a4d31941855f0174330d0ae35d71280c6ff6bcd0
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-04 18:04:51 -0400

    Create itemListContainer.js

---
+++ src/components/itemListContainer/itemListContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./itemListContainer.css"
+
+export default class ItemListContainer extends Component {
+    render() {
+        return (
+            <div className="listado">
+                <p>Aqui va un texto de relleno porque no se que poner</p>
+            </div>
+        )
+    }
+};
