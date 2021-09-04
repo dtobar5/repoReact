fb25b875086040f119a3f136cf2608313ad594c1
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-04 18:00:41 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ItemListContainer from './components/itemListContainer/itemListContainer';
+import NavBar from './components/navBar/navBar';
+
+const App = () => {
+  return <div>
+    <NavBar />
+    <ItemListContainer />
+  </div>
+};
+
+export default App;
+
+
