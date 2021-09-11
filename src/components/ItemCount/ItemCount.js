5ded6a90e82bb9a5774e8caac58c35e77da4b3dd
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-11 16:59:59 -0300

    Add files via upload

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import "./ItemCount.css"
+
+const ItemCount = () => {
+
+    let disabled1 = true;
+    let disabled2 = false;
+    const [counter, setCounter] = React.useState(0);
+
+    const sumar = () => {
+        setCounter((numero) => numero + 1)
+    }
+
+    const restar = () => {
+        setCounter((numero) => numero - 1)
+    }
+
+    if (counter === 0) {
+        disabled1 = true;
+        disabled2 = false;
+    } else if ((counter >= 1) && (counter <= 14)) {
+        disabled1 = false;
+        disabled2 = false;
+    } else if (counter === 15) {
+        disabled1 = false;
+        disabled2 = true;
+    }
+
+    return (
+        <div >
+            <div className="clicker">
+                <button className="btnClick" onClick={restar} disabled={disabled1}>-</button>
+                <p className="stock">Cantidad: {counter}</p>
+                <button className="btnClick" onClick={sumar} disabled={disabled2}>+</button>
+            </div>
+            <>
+                <p className="enCarrito">Stock maximo: 15 </p>
+            </>
+        </div >
+    )
+}
+
+export default ItemCount;
