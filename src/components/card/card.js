5584eedf755d67e46abe502b3ff270a298281c4f
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-12 16:35:26 -0300

    Add files via upload

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,13 +1,53 @@
 import React from 'react'
 import "./card.css"
 
-const Card = ({ title, src, description, mensaje }) => {
+
+const Card = ({ title, image, description }) => {
+
+
+    {/*
+    const productos = [
+        { id: 0, name: "mochila", price: 100 },
+        { id: 1, name: "prod2", price: 70 },
+        { id: 2, name: "prod3", price: 60 },
+        { id: 3, name: "prod4", price: 50 }
+    ]
+
+     const productos2 = [
+        { id: 0, title: "produ1", description: "descripcion del producto", image: "https://http2.mlstatic.com/D_Q_NP_683733-MLC45682674354_042021-AB.webp" },
+        { id: 1, title: "produ2", description: "descripcion del producto", image: "https://http2.mlstatic.com/D_Q_NP_683733-MLC45682674354_042021-AB.webp" },
+        { id: 2, title: "produ3", description: "descripcion del producto", image: "https://http2.mlstatic.com/D_Q_NP_683733-MLC45682674354_042021-AB.webp" },
+        { id: 3, title: "produ4", description: "descripcion del producto", image: "https://http2.mlstatic.com/D_Q_NP_683733-MLC45682674354_042021-AB.webp" }
+    ] */}
+
     return (
+
         <div className="card">
+
+            { /*
+        <ul className="fuente">
+            {productos.map((producto) => {
+                return <li key={producto.id}>{producto.name}</li>
+            })}
+        </ul>
+
+            <ul className="fuente">
+                {productos2.map((producto) => {
+                    return (
+                        <Card
+                            key={producto.id}
+                            title={producto.title}
+                            description={producto.description}
+                            image={producto.image}
+                        />
+                    )
+                })}
+            </ul> */ }
+
             <span className="tituloCard">{title}</span>
-            <img src={src} alt="imagen" className="imagenCard" />
+            <img src={image} alt="imagen" className="imagenCard" />
             <p className="descripcionCard">{description}</p>
-            <button className="btnCard" onClick={mensaje}>Mensaje</button>
+            <button className="btnCard">Al carrito</button>
         </div>
     )
 }
