da8bb90f4e8e943024cf509e5143c62fc4c21ed2
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-12 16:35:56 -0300

    Add files via upload

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import ItemList from '../ItemList/ItemList'
 import "./itemListContainer.css"
 
 export default class ItemListContainer extends Component {
     render() {
         return (
             <div className="listado">
-                <p>Aqui va un texto de relleno porque no se que poner</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nulla dui, sollicitudin a leo eu, mattis consequat ipsum.</p>
+
+                <ItemList />
             </div>
         )
     }
