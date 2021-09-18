7d1e5b9382aadee6dfdaad367692a1c514c33eeb
dtobar5 <84291032+dtobar5@users.noreply.github.com>
2021-09-18 17:21:40 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Card from './components/card/card';
 import Header from './components/header/header';
 import ItemCount from './components/ItemCount/ItemCount';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/itemListContainer/itemListContainer';
 import NavBar from './components/navBar/navBar';
 
@@ -19,6 +20,88 @@ const App = () => {
       .then((result) => setItems(result))
       .finally(() => setLoading(false));
   }, []);
+
+  ////////////////////////// poke api
+
+  const [poke, setPoke] = React.useState([]);
+
+  React.useEffect(() => {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
+    // devuelve una respuesta
+    fetch(url).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw response;
+      }
+    })
+      // trae la informacion solicitada y la guarda en data
+      .then((data) => setPoke(data.results))
+      //muestra error y la variable status del api
+      .catch((error) => console.log(`se pudrio todo,fue un error ${error.status}`));
+    // array de dependencias para que no se ejecute en loop, solo una vez
+  }, []);
+
+  ////////////////////////// db.json
+
+  const [dat, setDat] = React.useState([]);
+  const [cargando, setCargando] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+
+  React.useEffect(() => {
+    const url = "http://localhost:3001/products";
+    // devuelve una respuesta
+    setCargando(true);
+    fetch(url).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw response;
+      }
+    })
+      // trae la informacion solicitada y la guarda en data
+      .then((dat) => setDat(dat))
+      //muestra error y la variable status del api
+      .catch((error) => setError(error))
+      // array de dependencias para que no se ejecute en loop, solo una vez
+      .finally(() => setCargando(false));
+  }, []);
+
+  ////////////////////// se crea una funcion asyncrona (async away, donde se crea un nuevo producto con metodo post al presionar el boton en el error)
+
+  const postProducto = async () => {
+    const newProducto = {
+      "title": "CosaUno",
+      "description": "Esta es la descripción del producto cosa",
+      "price": 666420,
+      "image": "https://cdn1.static-tgdp.com/ui/productimages/approved/std.lang.all/62/64/696264_sized_1800x1200_rev_1.jpg"
+    };
+    const response = await fetch("http://localhost:3001/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProducto),
+    });
+    return response.json();
+  };
+
+
+  //////////////////////////
+
+  const [notDigimon, setNotDigimon] = React.useState([]);
+
+  React.useEffect(() => {
+    const urlDos = "https://pokeapi.co/api/v2/pokemon?limit=20";
+
+    fetch(urlDos).then(response => {
+      return response.json();
+    })
+      .then(datos => setNotDigimon(datos.results))
+      .catch(error => console.log("super error"));
+  }, []);
+
+  //////////////////////////
+
 
   const getProducts = () => {
     return new Promise((resolve) => {
@@ -38,11 +121,13 @@ const App = () => {
   // <> </> en vez del div App, se usa ese para que no se vea representado en html
   return (
 
-    <div className="App">
+    <div className="App" >
       <NavBar />
-      <Header name="Pepe Lota" />
+      <ItemDetailContainer />
+      <Header name="Pepito" />
       <ItemListContainer />
       <ItemCount />
+
 
       <div className="cardContainer">
 
@@ -80,7 +165,34 @@ const App = () => {
        /> */}
       </div>
 
-    </div>
+      <>
+        {notDigimon.map(pokemon => {
+          return <p className="fuente">{pokemon.name}</p>;
+        })};
+      </>
+
+      <>
+        <br></br>
+        <button onClick={postProducto} className="fuente">Cargar nuevo producto</button><br></br>
+        {cargando && <p className="fuente">Cargando...</p>}
+        {error && <p className="fuente">Ha habdo un error: {error.status} {error.statusText}</p>}
+        {dat?.map((product) => {
+          return (
+            <div className="cardContainer">
+              <ul>
+                <Card
+                  key={product.id}
+                  title={product.title}
+                  description={product.description}
+                  image={product.image}
+                />
+              </ul>
+            </div>
+          )
+        })};
+      </>
+
+    </div >
   )
 };
 
---
+++ src/ItemDetail/ItemDetail.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./ItemDetail.css"
+
+const ItemDetail = ({ id, title, price, pictureUrl, description }) => {
+
+
+    return (
+        <div className="itemDetail">
+
+            <span className="tituloItemDet">{title}</span>
+            <img src={pictureUrl} alt="imagen" className="imagenItemDet" />
+            <p className="precioItemDet">{price}</p>
+            <p className="descItemDet"></p>
+            <button className="detItem">Ver detalles</button>
+            <p className="descDetItem">{description}</p>
+        </div>
+    )
+}
+
+export default ItemDetail;
